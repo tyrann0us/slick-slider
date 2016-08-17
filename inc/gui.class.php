979f9d8b1e734873fca4533c74f2b1280e3e49cd
tyrann0us <pbammes@gmail.com>
2016-08-17 20:21:06 +0200

    Move help text to help tab, fixes #1

--- inc/gui.class.php
+++ inc/gui.class.php
@@ -15,7 +15,9 @@ class slickGui {
 	private static $skipSaving = false;
 
 	/**
-	 * Enqueues assets and prints Slick Slider options's HTML markup using add_settings_field().
+	 * Enqueues assets,
+	 * prints Slick Slider options's HTML markup using add_settings_field()
+	 * and adds a help tab to the Contextual Help menu.
 	 */
 	public static function initSettings() {
 
@@ -34,6 +36,14 @@ class slickGui {
 			array(
 				 __CLASS__,
 				'addJs' 
+			)
+		);
+
+		add_action(
+			'load-options-media.php',
+			array(
+				__CLASS__,
+				'addHelpTab'
 			)
 		);
 
@@ -81,7 +91,25 @@ class slickGui {
 	}
 
 	/**
-	 * Prints intro text and reset button
+	 * Adds help tab.
+	 */
+	public static function addHelpTab() {
+		$screen = get_current_screen();
+		$screen->add_help_tab( array(
+			'id' => 'slick-slider-help',
+			'title' => esc_html__( 'Slick Slider', 'slick-slider' ),
+			'content' => sprintf(
+				'<p>%s</p>',
+				sprintf(
+					__( 'For more information about the slider visit %s.', 'slick-slider' ),
+					'<a href="https://kenwheeler.github.io/slick/" target="blank">https://kenwheeler.github.io/slick/</a>'
+				)
+			)
+		) );
+	}
+
+	/**
+	 * Prints intro text and reset button.
 	 */
 	public static function settingSectionCallback() {
 
@@ -89,13 +117,6 @@ class slickGui {
 		echo '<a id="slick-settings"></a>';
 		echo '<input type="hidden" name="_slick_action" value="update" />';
 		echo sprintf( '<p>%s</p>', __( 'Change default Slick Slider settings.', 'slick-slider' ) );
-		echo sprintf(
-			'<p>%s</p>',
-			sprintf(
-				__( 'For more information about the slider visit %s.', 'slick-slider' ),
-				'<a href="https://kenwheeler.github.io/slick/" target="blank">https://kenwheeler.github.io/slick/</a>'
-			)
-		);
 		submit_button( __( 'Reset Slick Slider settings', 'slick-slider' ), 'delete', '_slick_reset' );
 
 	}
